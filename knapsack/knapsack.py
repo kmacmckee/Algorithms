49b2562cb84fd28b8c418bce16d29d0a30f31104
Kobe McKee <kobe.mckee@gmail.com>
2019-10-09 17:45:47 -0600

    recipe completed

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,9 +5,83 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+def quicksort(items):
+  if len(items) <= 1:
+    return items
+  pivot = items[-1]
+  left = []
+  right = []
+
+  for i in range(len(items) - 1):
+    item = items[i]
+    item_profit = item.value - item.size
+    pivot_profit = pivot.value - pivot.size
+    if item_profit > pivot_profit:
+      left.append(item)
+    else:
+      right.append(item)
+
+  return quicksort(left) + [pivot] + quicksort(right)
+
+
+
 def knapsack_solver(items, capacity):
-  pass
+  sorted_items = quicksort(items)
+  knapsack = []
+
+  while capacity > sorted_items[0].size:
+    new_item = sorted_items[0]
+    capacity -= new_item.size
+    knapsack.append(new_item)
+    sorted_items.remove(new_item)
+
+  weight = 0
+  value = 0
+  chosen = []
+  for item in knapsack:
+    weight += item.size
+    value += item.value
+    chosen.append(item.index)
+    #print(item.index)
+
+  print(f"weight: {weight}")
+  for item in sorted_items:
+    print(item.size)
+
+  return {'Value': value, 'Chosen': chosen}
+    
+
+
   
+  
+list1 = [Item(1, 42, 81),
+  Item(2, 42, 42),
+  Item(3, 68, 56),
+  Item(4, 68, 25),
+  Item(5, 77, 14),
+  Item(6, 57, 63),
+  Item(7, 17, 75),
+  Item(8, 19, 41),
+  Item(9, 94, 19),
+  Item(10, 34, 12)]
+
+list2 = [Item(1, 18, 100),
+  Item(2, 51, 76),
+  Item(3, 68, 26),
+  Item(4, 44, 79),
+  Item(5, 46, 64),
+  Item(6, 65, 18),
+  Item(7, 66, 1),
+  Item(8, 52, 80),
+  Item(9, 47, 93),
+  Item(10, 3, 66)]
+
+
+
+
+print(knapsack_solver(list1, 100))
+print(knapsack_solver(list2, 100))
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,38 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  rec_count = 0
+  ing_count = 0
+  print(recipe)
+
+  for item in recipe:
+    rec_count += 1
+
+  for item in ingredients:
+    ing_count += 1
+
+  if rec_count > ing_count:
+    return 0
+
+  total = 1000000
+  for r in recipe:
+
+    if int(ingredients[r] / recipe[r]) < total:
+      total = int(ingredients[r] / recipe[r])
+
+  return total
+    
+    
+      
+
+
+      
+
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 350, 'butter': 120, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
